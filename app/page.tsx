facdd2fdd3a09219375f407722865500dff414f4
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-12-03 13:22:29 -0500

    NavBar styling update

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ import NavBar from "./components/navbar";
 export default function Home() {
   return (
     <div>
+      <NavBar />
       <header className="text-4xl text-center font-bold p-8">
         Mayowa Awosiyan
       </header>
-      <NavBar />
+
       <br></br>
-      <div>
-        <header>About Me</header>
-        <p>
+      <div className="text-center gap-4">
+        <h1 className="text-center">About Me</h1>
+        <p className="text- center">
           I graduated from the University of Ottawa with a degree in computer
           science in June 2024. And I will add more to this later, including a
           navbar above this.
