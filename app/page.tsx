352c37c70365b597dfd0d56133327cac05cea330
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-12-09 12:48:41 -0500

    About me and Professional Summary

--- app/page.tsx
+++ app/page.tsx
@@ -14,9 +14,21 @@ export default function Home() {
       <div className="text-center gap-4">
         <h1 className="text-center">About Me</h1>
         <p className="text- center">
+          My name is Mayowa Awosiyan and I am a software developer. Welcome to
+          my portfolio, hopefully it impresses some of you! Currently I am
+          working on a project I am calling NestSplit which you can read about
+          here soon! When I am taking a break from updating this place or my
+          projects I am embracing my new found love for rouge-likes with Hades
+          or playing Marvel Rivals.
+        </p>
+        <h1 className="text-center">Professional History</h1>
+        <p className="text- center">
           I graduated from the University of Ottawa with a degree in computer
-          science in June 2024. And I will add more to this later, including a
-          navbar above this.
+          science in June 2024. During my degree I did multiple Co-ops as a web
+          developer with different departments in the Government of Canada and a
+          software developer for the University of Ottawa working under a
+          proffessor. For more details see the resume tab, which is currently a
+          work in progress!
         </p>
       </div>
     </div>
