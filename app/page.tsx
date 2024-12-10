16a7fe6e8ecf0b0f90e5c3ee8402d129fa5bd054
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-12-09 23:17:44 -0500

    Styling change

--- app/page.tsx
+++ app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
 
       <br></br>
       <div className="text-center gap-4">
-        <h1 className="text-center">About Me</h1>
-        <p className="text- center">
+        <h1 className="text-center font-bold text-xl">About Me</h1>
+        <p className="text- center p-8 px-12">
           My name is Mayowa Awosiyan and I am a software developer. Welcome to
           my portfolio, hopefully it impresses some of you! Currently I am
           working on a project I am calling NestSplit which you can read about
@@ -21,8 +21,8 @@ export default function Home() {
           projects I am embracing my new found love for rouge-likes with Hades
           or playing Marvel Rivals.
         </p>
-        <h1 className="text-center">Professional History</h1>
-        <p className="text- center">
+        <h1 className="text-center text-xl font-bold">Professional History</h1>
+        <p className="text- center p-8 px-12">
           I graduated from the University of Ottawa with a degree in computer
           science in June 2024. During my degree I did multiple Co-ops as a web
           developer with different departments in the Government of Canada and a
