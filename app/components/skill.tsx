4d7de9e567efdab6486f3799efbc1345722b4feb
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-12-15 18:24:52 -0500

    extra space removed

--- app/components/skill.tsx
+++ app/components/skill.tsx
@@ -18,7 +18,7 @@ export default function Skill({ link, alt, children }: SkillProps) {
     <button
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
-      className="bg-gray-700 p-4 hover:p-6 hover:self-center transition-all rounded-lg  flex-col items-center justify-center"
+      className="bg-gray-700 p-4 hover:p-6 hover:self-center transition-all rounded-lg flex-col items-center justify-center"
     >
       <Image
         className="place-content-center"
