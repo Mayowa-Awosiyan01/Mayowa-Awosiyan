d8436bd6507508db1ffd1f6422dfbe3be7c4fee8
Mayowa Awosiyan <oawos064@uottawa.ca>
2024-12-30 01:01:24 -0500

    Removed Resume Link

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -11,7 +11,7 @@ export default function NavBar() {
         </Link>
       </div>
       <div>
-        <Link href={"/resume"} className="flex-initial">
+        <Link href={"#"} className="flex-initial">
           Resume
         </Link>
       </div>
