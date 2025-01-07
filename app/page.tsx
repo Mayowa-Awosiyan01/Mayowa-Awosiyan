29f3b97218a2b5d341e0cb2c470574634ebf7f54
Mayowa Awosiyan <oawos064@uottawa.ca>
2025-01-07 15:06:15 -0500

    Spelling and Grammar fixes

--- app/page.tsx
+++ app/page.tsx
@@ -14,31 +14,30 @@ export default function Home() {
       <div className="text-center gap-4 m-6">
         <h1 className="text-center font-bold text-2xl">About Me</h1>
         <p className="text-center p-8 px-12">
-          My name is Mayowa Awosiyan and I am a software developer. Welcome to
+          My name is Mayowa Awosiyan, and I am a software developer. Welcome to
           my portfolio, which is still currently a work in progress but I hope
           it gives you an idea of my skills. I am also working on a project I am
           calling NestSplit which you can read about here soon! When I am taking
-          a break from updating this portfolio or my projects I am embracing my
-          new found love for rouge-likes with Hades or playing Marvel Rivals.
+          a break from updating this portfolio or my projects I am embracing me
+          newfound love for rouge-likes with Hades or playing Marvel Rivals.
         </p>
         <h1 className="text-center text-2xl font-bold">Professional History</h1>
         <p className="text-center p-8 px-12">
           I graduated from the University of Ottawa with a degree in computer
-          science in June 2024. During my degree I did multiple Co-ops as a web
+          science in June 2024. During my degree, I did multiple Co-ops as a web
           developer with different departments in the Government of Canada and a
           software developer for the University of Ottawa working under a
-          professor. For more details see the resume tab, which is currently a
-          work in progress!
+          professor. For more details see the resume tab.
         </p>
         <h1 className="text-center text-2xl font-bold">
           Technologies and Skills
         </h1>
         <p className="text-center p-8 px-12">
-          Over time I have learnt plenty of skills and technologies from my
-          Co-op positions, coursework and independent study. I am still learning
-          new skills and will definitely be adding to these in the future.
-          Currently I am learning cloud computing technologies such as Amazon
-          Web Services (AWS). For now however, the skills and tools I feel the
+          Over time, I have learned plenty of skills and technologies from my
+          Co-op positions, coursework, and independent study. I am still
+          learning new skills and will definitely add to these in the future.
+          Currently, I am learning cloud computing technologies such as Amazon
+          Web Services (AWS). For now, however, the skills and tools I feel the
           most comfortable with are the following:
         </p>
         <div className="grid md:grid-cols-5 mb-6 gap-4 justify-items-center sm:grid-cols-3">
--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -8,17 +8,17 @@ export default function Project() {
       <ParticleBackground />
       <h1 className="text-4xl text-center font-bold p-8">Projects</h1>
       <p className="text-center p-8 px-12">
-        Below you will find some descriptions of my projects, both completed
-        ones and works in progress. I will be updating this page as they
-        progress and as new ones start.
+        Below you will find some descriptions of my projects, some of which are
+        still in progress. I will be updating this page as they progress and as
+        new ones start.
       </p>
       <h3 className="text-center text-2xl font-semibold text p-8">NestSplit</h3>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
         <div className="p-4">
           <p>
-            NestSplit is my flutter project inspired by SplitWise. Splitwise is
+            NestSplit is my flutter project inspired by Splitwise. Splitwise is
             a mobile application that helps groups and households keep track of
-            who owes who money and how much. The goal for NestSplit is to create
+            who owes who money and how much. The goal of NestSplit is to create
             an application where households or &apos;nests&apos; can keep track
             of who owes who money, create shared grocery lists, keep track of
             chores and more to help families, roommates and others keep track of
@@ -66,18 +66,18 @@ export default function Project() {
         </div>
         <div className="p-4">
           <p>
-            For a course on artificial intelligence I was tasked with solving
+            For a course on artificial intelligence, I was tasked with solving
             the knapsack problem with ways involving algorithms used in
-            Artificial Intelligence and comparing the results to the more
+            artificial intelligence and comparing the results to the more
             traditional ways of solving the problem. <br></br>
             The knapsack problem is a problem where you are given a group of
-            items each with their own weight and value alongside a knapsack with
-            a maximum weight. Your task is to find the combination of items that
+            items each with a weight and a value alongside a knapsack with a
+            maximum weight. Your task is to find the combination of items that
             maximizes total value without exceeding the max weight of the
             knapsack. In this report I solved instances of the problem using
-            simmulated annealing and the genetic algorithm to utilize concepts
-            of artificail intelligence and machine learning which was the focus
-            of the course.
+            simulated annealing and the genetic algorithm to utilize concepts of
+            artificial intelligence and machine learning which was the focus of
+            the course.
           </p>
         </div>
       </div>
@@ -91,10 +91,11 @@ export default function Project() {
             skills and experience. I developed it using Next.JS so the frontend
             is React, HTML, JavaScript and TailwindCSS. Meanwhile it is deployed
             with Vercel. The animated background was inspired by the Matrix and
-            was created using the JavaScript library react-particles-js. Some
-            things I want to add to this website include: a button to change the
-            animated background to different styles and my resume so please
-            check back for those updates in the future.
+            created using the JavaScript library react-particles-js. Some things
+            I want to add to this website include: a button to change the
+            animated background to different styles and changing the skills on
+            the homepage so when they are clicked they give a more detailed
+            picture on how I have used the skill.
           </p>
         </div>
         <div className="p-4">
