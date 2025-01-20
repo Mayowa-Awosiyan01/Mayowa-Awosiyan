f0854565bc0f1fa8b64c8df6e08964dd5c739d30
Mayowa Awosiyan <oawos064@uottawa.ca>
2025-01-20 00:07:35 -0500

    Button Formatting

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -2,7 +2,7 @@ import Form from "next/Form";
 //Make this bigger and add a Contact Me! header
 export default function Contact() {
   return (
-    <div className="p-4">
+    <div className="p-4 flex flex-col">
       <Form action={""}>
         <p className="text-center text-white text-lg">
           What made you visit my website? Do you have any comments? Share them
@@ -12,7 +12,14 @@ export default function Contact() {
           className="resize-none size-2/3 min-h-16 placeholder:text-black text-black"
           placeholder="What made you visit this site? Any feedback/suggestions/comments etc?"
         ></textarea>
-        <button type="submit"></button>
+        <div>
+          <button
+            className="rounded-full font-bold bg-blue-500 hover:bg-blue-900 px-6 py-2 ring-white/50 m-4"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
       </Form>
     </div>
   );
