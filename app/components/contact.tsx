f5ac68fe3000b8333499c9078b1b6dd93c9286cd
Mayowa Awosiyan <oawos064@uottawa.ca>
2025-01-20 12:35:17 -0500

    Form fix

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -1,15 +1,14 @@
-import Form from "next/Form";
 //Make this bigger and add a Contact Me! header
 export default function Contact() {
   return (
     <div className="p-4 flex flex-col">
-      <Form action={""}>
+      <form action={""}>
         <p className="text-center text-white text-lg">
           What made you visit my website? Do you have any comments? Share them
           below!
         </p>
         <textarea
-          className="resize-none size-2/3 min-h-16 placeholder:text-black text-black"
+          className="resize-none size-2/3 min-h-16 placeholder:text-black text-black text-lg"
           placeholder="What made you visit this site? Any feedback/suggestions/comments etc?"
         ></textarea>
         <div>
@@ -20,7 +19,7 @@ export default function Contact() {
             Submit
           </button>
         </div>
-      </Form>
+      </form>
     </div>
   );
 }
