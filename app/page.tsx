eb180d4f676d1dc84d612d45dfbee25f4cf0a530
Mayowa Awosiyan <oawos064@uottawa.ca>
2025-04-22 13:39:40 -0400

    removed snap stylings

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import ParticleBackground from "./components/particlebackground";
 import Project from "./components/project";
 import Skill from "./components/skill";
 import Image from "next/image";
-import WelcomeSection from "./components/welcomeSection";
 
 interface ProjectType {
   link: string;
@@ -21,7 +20,6 @@ export default function Home() {
 
   const handleProjectSelect = (project: ProjectType) => {
     console.log(project.prompt.toString());
-    // Toggle project selection
     setSelectedProject((prevProject) =>
       prevProject?.prompt.toString() === project.prompt.toString()
         ? null
@@ -126,7 +124,7 @@ export default function Home() {
     },
   ];
   return (
-    <div className="snap-y snap-mandatory scroll-smooth h-screen">
+    <div className="h-screen">
       <ParticleBackground />
 
       <header className="text-4xl text-center font-bold p-8">
@@ -134,7 +132,7 @@ export default function Home() {
       </header>
 
       <br></br>
-      <div className="text-center gap-4 m-6 snap-start">
+      <div className="text-center gap-4 m-6">
         <h1 className="text-center font-bold text-2xl">About Me</h1>
         <p className="text-center p-8 px-12 text-lg">
           My name is Mayowa Awosiyan, and I am a software developer. Welcome to
